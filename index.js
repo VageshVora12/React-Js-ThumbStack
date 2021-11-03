c662ea8395080f4fa8fccabceaef30f83ba1b45a
VageshVora12 <92394950+VageshVora12@users.noreply.github.com>
2021-11-03 23:22:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-
-// all tasks are stored in todaystaskssavedroughly.js so to run all of them copy each one of them one at a time 
-// and paste here to view the output of the program in the browser;
+// all tasks are stored seperately so to run all of them copy each one of them one at a time 
+// and paste in index.js to view the output of the program in the browser;
 
 
